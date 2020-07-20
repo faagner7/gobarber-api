4e129a7b01306781683a750aefb20c2cc8f252c3
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2020-07-20 02:13:22 -0300

    fix default uuid generate on migration CreateAppointments

--- src/database/migrations/1595216851448-CreateAppointments.ts
+++ src/database/migrations/1595216851448-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1595216851448
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
