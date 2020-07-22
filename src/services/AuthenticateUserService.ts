71d70c719a74f1e2a2429e772494fd0568cd1fa1
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2020-07-22 18:42:44 -0300

    Add session to project

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,50 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    // user.password - Senha criptografada.
+    // password - Senha não-criptograda
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, '2a820ah29haah09uhd', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
